7e3c5443d4b4a1892cce88741a86bf1761ce8313
Liam Sullivan <30022060@student.toiohomai.ac.nz>
2019-08-01 10:28:34 +1200

    yes

---
+++ UML task 1/UML task 1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+
+            string check = "y";
+            while (check == "y")
+            {
+                Person.counter++;
+                Person newPerson = new Person();
+                Console.Write("Enter person's name: ");
+                newPerson.Fname = Console.ReadLine();
+                Console.Write("Enter person's last name: ");
+                newPerson.Lname = Console.ReadLine();
+                Console.Write("Enter person's age: ");
+                newPerson.Age = Console.ReadLine();
+                people.add(newPerson); ;
+                Console.Write("Add another?");
+                check = Console.ReadLine();
+            }
+
+            Console.Write($"No. of people created: {Person.counter}");
+            foreach (Person x in people) ;
+            Console.WriteLine($"Name: {x.Fname} {x.Lname}\n Age: {x.Age}");
+                      
+        }
+
+        List<Person> peopl = new List<Person>()
+        {
+            new Person("Bob","Morris",60),
+            new Person("Tim","Morris",60),
+            new Person("Jack","Morris",60),
+        };
+
+
+
+
+      
+    }
+    class Person
+    {
+        private string fname;
+        private string lname;
+        private int age;
+        private static int counter;
+
+        public string Fname { get { return fname; } set { fname = value; } }
+        public string Lname { get { return lname; } set { lname = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public string ReturnDeets()
+        {
+            return $"First Name: {fname}\nLast Name: {Lname}\nAge: {Age}";
+        }
+    }
+}
---
+++ UML task 2/UML task 2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tcheck = true;
+            bool fcheck = false;
+            do
+            {
+                ShoppingCart s1 = new ShoppingCart();
+                Console.WriteLine("Enter Username: ");
+                s1.Username = Console.ReadLine();
+                Console.WriteLine("Enter Password: ");
+                s1.Password = Console.ReadLine();
+
+                do
+                {
+                    Console.WriteLine("Add items to Cart: ");
+                    s1.Cart.Add(Console.ReadLine());
+                    Console.WriteLine("Please enter price of item");
+                    s1.CartTotal += double.Parse(Console.ReadLine());
+                } while (fcheck);
+                Console.WriteLine("Add another item? y/n: ");
+                string input = Console.ReadLine().ToLower();
+                if (input == "n")
+                    fcheck = false;
+
+            } while (tcheck);
+
+            
+
+        } 
+    }
+    class ShoppingCart
+    {
+        private string username;
+        private string password;
+        private string cartTotal;
+        List<string> cart = new List<string>();
+
+        public string Username { get { return username; } set { username = value; } }
+        public string Password { get { return password; } set { password = value; } }
+        public string CartTotal { get { return cartTotal; } set { cartTotal = value; } }
+        public List<string> Cart { get { return cart; } set { cart = value; } }
+
+    }
+}
